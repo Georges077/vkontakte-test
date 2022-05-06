cc1a1a43e865502109d0477210bd8fa22243e5a4
Georges077 <giomaxvili@gmail.com>
2022-05-06 11:32:40 +0400

    Add files via upload

---
+++ search_facebook.py
@@ -0,0 +1,208 @@
+from __future__ import annotations
+from datetime import datetime
+import requests
+import pandas as pd
+import os
+from uuid import UUID
+
+from typing import List, Dict
+
+from ibex.models.account import Account
+from ibex.models.search_term import SearchTerm
+from ibex.models.post import Post, Scores
+from ibex.models.platform import Platform
+from ibex.models.collect_task import CollectTask
+
+
+# from app.core.datasources.facebook.helper import split_to_chunks, needs_download
+# from app.core.datasources.utils import update_hits_count
+
+class FacebookCollector:
+    def __init__(self, *args, **kwargs):
+        self.token = os.getenv('CROWDTANGLE_TOKEN')
+
+        # TODO: double check the limit per post
+        self.max_posts_per_call = 100
+        self.max_requests = 20
+
+        self.max_posts_per_call_sample = 50
+        self.max_requests_sample = 1
+
+    @staticmethod
+    def _collect_posts_by_param(params):
+        res = requests.get("https://api.crowdtangle.com/posts", params=params).json()
+        return res
+
+    def generate_request_params(self, collect_task: CollectTask):
+        self.max_requests_ = self.max_requests_sample if collect_task.sample else self.max_requests
+        self.max_posts_per_call_ = self.max_posts_per_call_sample if collect_task.sample else self.max_posts_per_call
+
+        params = dict(
+            token=self.token,
+            startDate=collect_task.date_from.isoformat(),
+            endDate=collect_task.date_to.isoformat(),
+            count=self.max_posts_per_call_
+        )
+
+        if collect_task.query is not None and len(collect_task.query) > 0:
+            params['searchTerm'] = collect_task.query
+        if collect_task.accounts is not None and len(collect_task.accounts) > 0:
+            params['accounts'] = ','.join([account.platform_id for account in collect_task.accounts])
+
+        return params
+
+    async def collect(self, collect_task: CollectTask) -> List[Post]:
+        params = self.generate_request_params(collect_task)
+
+        results: List[any] = self._collect(params)
+        posts = self._map_to_posts(results, params)
+
+        return posts
+
+    def _collect(self, params) -> List[Dict]:
+        results = []
+        res = {"result": {"pagination": {"nextPage": None}}}
+        offset = 0
+
+        while 'nextPage' in res["result"]["pagination"]:
+            params["offset"] = self.max_posts_per_call_ * offset
+            offset += 1
+            res = self._collect_posts_by_param(params)
+
+            if "result" not in res or "posts" not in res["result"]:
+                self.log.warn(f'[Facebook] result not present in api response, breaking loop..')
+                break
+
+            results += res["result"]["posts"]
+
+            if offset >= self.max_requests_:
+                self.log.success(f'[Facebook] limit of {self.max_requests}')
+                #  f' requests has been reached for params: {params}.')
+                break
+
+        if not len(results):
+            self.log.warn('[Facebook] No data collected')
+            return results
+
+        self.log.success(f'[Facebook] {len(results)} posts collected')
+
+        return results
+
+    async def get_hits_count(self, collect_task: CollectTask) -> int:
+        params = self.generate_request_params(collect_task)
+        params['count'] = 0
+
+        hits_count = requests.get("https://api.crowdtangle.com/posts/search", params=params).content
+
+        self.log.info(f'[Facebook] Hits count - {hits_count}')
+
+        return hits_count
+
+    @staticmethod
+    def map_to_post(api_post: Dict, collect_task: CollectTask) -> Post:
+        # create scores class
+        scores = None
+        if 'statistics' in api_post and 'actual' in api_post['statistics']:
+            actual_statistics = api_post['statistics']['actual']
+            likes = actual_statistics['likeCount'] if 'likeCount' in actual_statistics else None
+            shares = actual_statistics['shareCount'] if 'shareCount' in actual_statistics else None
+            love_count = actual_statistics['loveCount'] if 'loveCount' in actual_statistics else None
+            wow_count = actual_statistics['wowCount'] if 'wowCount' in actual_statistics else None
+            sad_count = actual_statistics['sadCount'] if 'sadCount' in actual_statistics else None
+            angry_count = actual_statistics['angryCount'] if 'angryCount' in actual_statistics else None
+            engagement = actual_statistics['commentCount'] if 'commentCount' in actual_statistics else None
+            scores = Scores(likes=likes,
+                            shares=shares,
+                            love=love_count,
+                            wow=wow_count,
+                            sad=sad_count,
+                            angry=angry_count,
+                            engagement=engagement)
+
+        # create post class
+        title = ""
+        if 'title' in api_post:
+            title = api_post['title']
+        elif 'message' in api_post:
+            title = api_post['message']
+
+        url = api_post['postUrl'] if 'postUrl' in api_post.keys() else None
+
+        post_doc = Post(title=api_post['message'] if 'message' in api_post else "",
+                        text=api_post['description'] if 'description' in api_post else "",
+                        created_at=api_post['date'] if 'date' in api_post else datetime.now(),
+                        platform=Platform.facebook,
+                        platform_id=api_post['platformId'],
+                        author_platform_id=api_post['account']['id'] if 'account' in api_post else None,
+                        scores=scores,
+                        api_dump=api_post,
+                        monitor_id=collect_task.monitor_id,
+                        url=url
+                        )
+        return post_doc
+
+    def _map_to_posts(self, posts: List[Dict], collect_task: CollectTask):
+        res: List[Post] = []
+        for post in posts:
+            try:
+                post = self.map_to_post(post, collect_task)
+                res.append(post)
+            except ValueError as e:
+                self.log.error(f'[{collect_task.platform}] {e}')
+        return res
+
+    async def get_accounts(self, collect_task: CollectTask) -> List[Account]:
+        params = self.generate_acc_req_params(collect_task)
+        res = requests.get("https://graph.facebook.com/pages/search", params)
+        acc = res.json()['items']
+        accounts = self.map_to_accounts(acc, collect_task)
+        return accounts
+
+    def generate_acc_req_params(self, collect_task: CollectTask):
+        params = dict(
+            q=collect_task.query,
+            fields=['id', 'name', 'location', 'link'],
+            access_token=self.token,
+        )
+        return params
+
+    def map_to_accounts(self, accounts: List, collect_task: CollectTask) -> List[Account]:
+        result: List[Account] = []
+        for account in accounts:
+            try:
+                account = self.map_to_acc(account, collect_task)
+                result.append(account)
+            except ValueError as e:
+                print({collect_task.platform}, e)
+        return result
+
+    def map_to_acc(self, acc: Account, collect_task: CollectTask) -> Account:
+        mapped_acc = Account(
+            title=acc['name'],
+            url=acc['link'],
+            platform=Platform.facebook,
+            platform_id=acc['id'],
+        )
+        return mapped_acc
+
+# async def test():
+#     ibex_models.platform import Platform
+#     from app.config.mongo_config import init_mongo
+#     await init_mongo()
+#     date_from = datetime.now() - timedelta(days=5)
+#     date_to = datetime.now() - timedelta(days=1)
+#     accounts = await Account.find(Account.platform == Platform.facebook).to_list()
+#     fb = FacebookCollector()
+#     res = fb.collect_curated_batch(date_from=date_from.isoformat(),
+#                                     date_to=date_to.isoformat(),
+#                                     accounts=accounts)
+#     print(res)
+#
+#
+# if __name__ == "__main__":
+#     import asyncio
+#
+#     asyncio.run(test())
+
+
+# os('python3 sample.py monitor_id=128376-81723618-087186238712 sample=True')
---
+++ search_telegram.py
@@ -0,0 +1,253 @@
+import os
+from datetime import datetime
+from telethon.sync import TelegramClient
+from telethon import functions
+from typing import List, Dict
+from ibex.models.post import Post, Scores
+from ibex.models.collect_task import CollectTask
+from ibex.models.platform import Platform
+from ibex.models.account import Account
+from abc import ABC
+
+
+class TelegramCollector(ABC):
+    """The class for data collection from TelegramClient.
+
+        All data collectors/data sources implement
+        methods described below.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # hash parameter for authorization.
+        self.hash = os.getenv('TELEGRAM_HASH')
+
+        # id parameter for authorization.
+        self.id = os.getenv('TELEGRAM_ID')
+
+        # TODO: double check the limit per post
+        # Variable for maximum number of posts per request
+        self.max_posts_per_call = 10000
+
+        # Variable for maximum number of requests
+        self.max_requests = 20
+
+    def generate_req_params(self, collect_task: CollectTask):
+        params = dict(
+            count=self.max_posts_per_call,
+        )
+        if collect_task.query is not None and len(collect_task.query) > 0:
+            params['q'] = collect_task.query
+        if collect_task.accounts is not None and len(collect_task.accounts) > 0:
+            params['groups'] = ','.join([account.platform_id for account in collect_task.accounts])
+
+        return params
+
+    async def collect(self, collect_task: CollectTask) -> List[Post]:
+        """The method is responsible for collecting posts
+            from platforms.
+
+        Args:
+            collect_action(CollectTask): CollectTask object holds
+                all the metadata needed for data collection.
+
+        Returns:
+            (List[Post]): List of collected posts.
+        """
+
+        # Dict variable for generated meta data.
+        params = self.generate_req_params(collect_task)
+
+        # Variable for TelegramClient instance
+        client = TelegramClient('username', self.id, self.hash)
+        await client.start()
+
+        # List variable for all posts data.
+        posts = []
+
+        # Boolean variable for looping through pages.
+        next_from = True
+        dialog_name = ''
+        if collect_task.accounts:
+            dialog_name = collect_task.accounts[0].platform_id
+
+        # Variables for searching through date range.
+        pre_first_msg = await client.get_messages(dialog_name, offset_date=collect_task.date_from, limit=1)
+        first_msg = await client.get_messages(dialog_name, min_id=pre_first_msg[0].id, limit=1)
+        last_msg = await client.get_messages(dialog_name, offset_date=collect_task.date_to, limit=1)
+
+        offset = 0
+        while next_from:
+            messages = await client.get_messages(dialog_name,
+                                                 search=collect_task.query,
+                                                 min_id=last_msg[0].id,
+                                                 max_id=first_msg[0].id,
+                                                 add_offset=offset,
+                                                 limit=self.max_posts_per_call
+                                                 )
+
+            if len(messages) < params['count']:
+                next_from = False
+
+            offset += params['count']
+            posts += messages
+
+        maped_posts = self._map_to_posts(posts, collect_task)
+        print(f'{len(maped_posts)} posts collected from dialog: {dialog_name}')
+        await client.disconnect()
+        return maped_posts
+
+
+
+    async def get_hits_count(self, collect_task: CollectTask) -> int:
+        """The method is responsible for collecting the number of posts,
+            that satisfy all criterias in CollectTask object.
+
+        Note:
+            Do not collect actual posts here, this method is only
+            applicable to platforms that provide this kind of information.
+
+        Args:
+            collect_action(CollectTask): CollectTask object holds
+                all the metadata needed for data collection.
+
+        Returns:
+            (int): Number of posts existing on the platform.
+        """
+
+        # Dict variable for generated meta data.
+        params = self.generate_req_params(collect_task)
+
+        # Variable for TelegramClient instance
+        client = TelegramClient('name', self.id, self.hash)
+        await client.start()
+
+        # Boolean variable for looping through pages.
+        next_from = True
+
+        # Variable for storing number of posts.
+        hits_count = 0
+
+        # Variable for getting all the channels or chats for user.
+        dialog_name = ''
+        if collect_task.accounts:
+            dialog_name = collect_task.accounts[0].platform_id
+        # Variables for searching through date range.
+        pre_first_msg = await client.get_messages(dialog_name, offset_date=collect_task.date_from, limit=1)
+        first_msg = await client.get_messages(dialog_name, min_id=pre_first_msg[0].id, limit=1)
+        last_msg = await client.get_messages(dialog_name, offset_date=collect_task.date_to, limit=1)
+
+        offset = 0
+        while next_from:
+            messages = await client.get_messages(dialog_name, search=collect_task.query, min_id=last_msg[0].id,
+                                                 max_id=first_msg[0].id, add_offset=offset,
+                                                 limit=params['count'])
+            if len(messages) < params['count']:
+                hits_count = offset + len(messages)
+                next_from = False
+            offset = offset + params['count']
+        await client.disconnect()
+        return hits_count
+
+    @staticmethod
+    def map_to_post(api_post: Dict, collect_task: CollectTask) -> Post:
+        """The method is responsible for mapping data redudned by plarform api
+            into Post class.
+
+        Args:
+            api_post: responce from platform API.
+            collect_action(CollectTask): the metadata used for data collection task.
+
+        Returns:
+            (Post): class derived from API data.
+        """
+
+        scores = Scores(
+            likes=api_post.replies, # Temporarily saved to likes.
+            shares=api_post.forwards,
+        )
+        post_doc = ''
+        try:
+            post_doc = Post(title="post" if 'post' in str(api_post) else "",
+                            text=api_post.message if 'message' in str(api_post) else "",
+                            created_at=api_post.date if 'date' in str(api_post) else datetime.now(),
+                            platform=Platform.telegram,
+                            platform_id=api_post.from_id if api_post.from_id is not None else '',
+                            author_platform_id=api_post.peer_id.channel_id if 'channel_id' in str(api_post) else None,
+                            scores=scores,
+                            api_dump=dict({"dump": str(api_post)}),
+                            monitor_id=api_post.id,
+                            # url=url,
+                            )
+        except Exception as exc:
+            print(exc)
+        return post_doc
+
+    def _map_to_posts(self, posts: List[Dict], collect_task: CollectTask):
+        res: List[Post] = []
+        for post in posts:
+            try:
+                post = self.map_to_post(post, collect_task)
+                res.append(post)
+            except ValueError as e:
+                self.log.error(f'[{collect_task.platform}] {e}')
+        return res
+
+    def generate_params(self, collect_task: CollectTask):
+        params = dict(
+            limit=5,
+        )
+        if collect_task.query is not None and len(collect_task.query) > 0:
+            params['q'] = collect_task.query
+        # if collect_task.accounts is not None and len(collect_task.accounts) > 0:
+        #     params['groups'] = ','.join([account.platform_id for account in collect_task.accounts])
+
+        return params
+
+    async def get_accounts(self, collect_task: CollectTask) -> List[Account]:
+        # Dict variable for generated meta data.
+        params = self.generate_params(collect_task)
+
+        # Variable for TelegramClient instance
+        client = TelegramClient('username', self.id, self.hash)
+        await client.start()
+
+
+        dialogs = await client(functions.contacts.SearchRequest(
+            q=params['q'],
+            limit=params['limit'],
+        ))
+
+        # List variable for all accounts data.
+        accounts = self.map_to_accounts(dialogs.chats, collect_task)
+
+        return accounts
+
+    def map_to_accounts(self, accounts: List, collect_task: CollectTask) -> List[Account]:
+        """The method is responsible for mapping data redudned by plarform api
+                   into Account class.
+
+               Args:
+                   accounts: responce from platform API.
+                   collect_action(CollectTask): the metadata used for data collection task.
+
+               Returns:
+                   (Account): class derived from API data.
+               """
+        result: List[Account] = []
+        for account in accounts:
+            try:
+                account = self.map_to_acc(account, collect_task)
+                result.append(account)
+            except ValueError as e:
+                print({collect_task.platform}, e)
+        return result
+
+    def map_to_acc(self, acc: Account, collect_task: CollectTask) -> Account:
+        mapped_account = Account(
+            title=acc.title,
+            url='t.me/'+acc.username,
+            platform=Platform.telegram,
+            platform_id=acc.id,
+            broadcasting_start_time=acc.date
+        )
+        return mapped_account
